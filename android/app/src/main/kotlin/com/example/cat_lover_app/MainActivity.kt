6c57cb1feb1650b32fdc62cfd60cc09a397c934f
Ivan Cardona <2021ivancardona@gmail.com>
2025-10-16 22:22:20 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/cat_lover_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cat_lover_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:cat_lover_app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
